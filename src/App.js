0e8cbcc3b60fd9869becacbe1500cdaf87b69020
sreenand <58810499+sreenand-murali@users.noreply.github.com>
2023-09-09 01:25:52 +0530

    to deploy to gp

--- src/App.js
+++ src/App.js
@@ -10,15 +10,17 @@ import Contact from './Contact';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <BrowserRouter>
+      
+      <h1>heloooo</h1>
+      {/* <Navbar/>
+      <BrowserRouter basename='/GCEK-GDSCFrontend'>
         <Routes>
           <Route path="/" element={<Homepage/>}/>
           <Route path="/about" element={<About/>}/>
           <Route path="/contact" element={<Contact/>}/>
           <Route path="/gcsj" element={<GoogleCloudJam/>}/>
         </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
       
     </div>
   );
